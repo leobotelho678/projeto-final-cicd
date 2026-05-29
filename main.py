047ca8a20204337ffa8435b5cf10432681abb808
Leonardo <leobotelho678@gmail.com>
2026-05-29 18:43:00 -0300

    test: inserindo vulnerabilidade proposital

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ def saudacao(nome):
 
 if __name__ == "__main__":
     print(saudacao("FATEC"))
+
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+# Código malicioso adicionado de propósito para teste
+def calculadora_vulneravel(expressao):
+    return eval(expressao)
+
+if __name__ == "__main__":
+    print(saudacao("FATEC"))
