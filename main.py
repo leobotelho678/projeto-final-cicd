1a22cc6d285ae36a5f71c0ebb365c5c9ebe8a235
Leonardo <leobotelho678@gmail.com>
2026-05-29 18:57:00 -0300

    test: forcando alerta do codeql com input

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ def saudacao(nome):
 if __name__ == "__main__":
     print(saudacao("FATEC"))
 
+
+
 def saudacao(nome):
     return f"Olá, {nome}!"
 
-# Código malicioso adicionado de propósito para teste
-def calculadora_vulneravel(expressao):
-    return eval(expressao)
+# O CodeQL agora vai detectar que o usuário pode digitar código malicioso
+def calculadora_vulneravel():
+    entrada_usuario = input("Digite a expressão: ")
+    return eval(entrada_usuario)
 
 if __name__ == "__main__":
     print(saudacao("FATEC"))
